71b7aaf61379fe17ddf14d8ecfbfe0708b8e7fbd
amitkumarx86 <amitkumarx86@gmail.com>
2016-09-17 03:44:51 +0530

    NetWorking

--- IIITB/src/Algorithms/NumbersfromOnetoK.java
+++ IIITB/src/Algorithms/NumbersfromOnetoK.java
@@ -5,57 +5,55 @@ public class NumbersfromOnetoK {
 	NumbersfromOnetoK m = new NumbersfromOnetoK();
 	m.findSubArrayWith_1_to_K_Numbers();
     }
+
     private void findSubArrayWith_1_to_K_Numbers(){
-	int[] arr={10,6,3,7,6,4,2,8,6,1,2,7,7,1,2,3,4,7,3,4,7,7,7,1,2,9,9,9,9,9,9};
-	//System.out.println(arr.length);
+	//int[] arr={10,6,3,7,6,4,2,8,6,1,2,7,7,1,2,3,4,7,3,4,7,7,7,1,2,9,9,9,9,9,9};
+	int arr[] = {1, 2, 3, 4,4, 5, 6, 1, 7, 1, 2, 4, 5, 3, 6, 7, 10};
 	int k=4,strtInd=0,endInd=0,digCount=0;
 	int[] kArr=new int[k+1];
-
-	/*
+        /*
 	 * First find the sub array in which 1 to k number exist, it may 
-	 * not be the minimal then we will try to minimize it
+	 * not be the minimal
 	 * */
 	for(int i=0;i<arr.length;i++){  
 	    if(arr[i] <=k && arr[i] >=1){
-		if(kArr[arr[i]]==0) digCount++;
+		if(kArr[arr[i]]==0) digCount++;    // count of digits from 1...k
 		kArr[arr[i]]++;
 		if(digCount == k) {endInd=i;break;}
 	    }
 	}
 	
-	int min_len=endInd-strtInd+1,tempInd=endInd;
+	int min_len=endInd-strtInd+1; // initially we let this is min 
+        int tempInd=endInd;
 	int i=strtInd;
-	//System.out.println(" min_len="+min_len);
-	while(tempInd<arr.length){
+	
+        while(tempInd<arr.length){
 	    if(arr[i]<=k){
-		//System.out.println("i="+i+" arr[i]:"+arr[i]+" tempInd="+tempInd+" arr[tempInd]:"+arr[tempInd]);
 		kArr[arr[i]]--;
-		//printArray(kArr);
 		if(kArr[arr[i]] == 0){
 		    kArr[arr[i]]++;
+                    // calculating new minimum
 		    if(tempInd-i+1 < min_len){
 			min_len=tempInd-i+1;
 			strtInd=i;endInd=tempInd;
-			//System.out.println(" min_len="+min_len);
 		    }
-		    //printArray(kArr);
 		    if(tempInd < arr.length)
 			tempInd++;
+                    // skipping nos which are not in range 1..k
 		    while(tempInd < arr.length && arr[tempInd] > k)tempInd++;
-		    if(tempInd < arr.length)
+		    // if no is in 1..k then increment its count in kArray
+                    if(tempInd < arr.length)
 			kArr[arr[tempInd]]++;
-		    //printArray(kArr);
-		    //System.out.println(tempInd);
-		    //System.out.println(min_len+" "+(tempInd-i+1));
 		}
 		else
-		    	i++;
+		    	i++; // increase i when the dig count in not reduced
 	    }
 	    else
-		i++;
+		i++; // increase i if no is out of range 1..k
 	    
 	}
-	System.out.println(strtInd+" "+(endInd));
+        //print start and end index of sub array
+	System.out.println(strtInd+" "+(endInd)); // final answer
     }
     private void printArray(int[] arr){
 	for(int a : arr) System.out.print(a+" ");
